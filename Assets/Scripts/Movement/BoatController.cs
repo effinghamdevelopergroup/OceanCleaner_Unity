254874cb5a7eb4c70ad2c9bff014b9fef2a2765e
Joey Meyer <joeymeyer1986@gmail.com>
2020-02-01 02:50:25 -0600

    added click movement script.

---
+++ Assets/Scripts/Movement/BoatController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    public float Speed;
+    public Vector3 Position;
+    public CharacterController Controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            LocatePosition();
+        }
+
+        MoveToPosition();
+    }
+
+    private void MoveToPosition()
+    {
+        if(Vector3.Distance(transform.position, Position) > 1)
+        {
+            Quaternion rotation = Quaternion.LookRotation(Position - transform.position, Vector3.forward);
+
+            rotation.x = 0f;
+            rotation.z = 0f;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime + 10);
+            Controller.SimpleMove(transform.forward * Speed);
+        }
+    }
+
+    private void LocatePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray , out hit, 1000))
+        {
+            Position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            Debug.Log(Position);
+        }
+    }
+}
