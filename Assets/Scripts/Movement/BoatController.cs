759e74d65bd321551351c250b67226fa3341e56c
Logan Malcome <lmalcome@lakelandcollege.edu>
2020-02-01 11:31:08 -0600

    Changed controls to keyboard

--- Assets/Scripts/Movement/BoatController.cs
+++ Assets/Scripts/Movement/BoatController.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
+    public bool Turning = false;
     public float Speed;
+    public float MaxSpeed;
+    public float Acceloration;
+    public float Decceloration;
     public Vector3 Position;
     public Rigidbody Body;
+    private float CurrentSpeed;
+    public GameObject target;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Target");
     }
 
     // Update is called once per frame
@@ -20,33 +27,66 @@ public class BoatController : MonoBehaviour
     {
         if (Input.GetAxis("Vertical") > 0)
         {
-            Body.AddForce(Vector3.forward, ForceMode.Impulse);
+            CurrentSpeed = Mathf.Lerp(CurrentSpeed, MaxSpeed, Acceloration);
         }
 
-        if (Input.GetAxis("Vertical") < 0)
+        else if (Input.GetAxis("Vertical") < 0)
         {
-            Body.AddForce(Vector3.forward * -1, ForceMode.Impulse);
+            CurrentSpeed = Mathf.Lerp(CurrentSpeed, -MaxSpeed, Acceloration);
+        }
+        else
+        {
+            CurrentSpeed = Mathf.Lerp(CurrentSpeed, 0, Decceloration);
         }
 
         if (Input.GetMouseButton(0))
         {
+            
+            Turning = transform;
             LocatePosition();
         }
 
-        RotateTowardPosition();
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(0, Time.deltaTime * turnSpeed,0);
+        }
+
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Rotate(0, -Time.deltaTime * turnSpeed, 0);
+        }
+
+        UpdatePosition();
+
+        //RotateTowardPosition();
+    }
+
+    private void UpdatePosition()
+    {
+        transform.Translate(Vector3.forward * CurrentSpeed);
     }
 
     private void RotateTowardPosition()
     {
-        //if(Vector3.Distance(transform.position, Position) > 1)
-        //{
+        if (Turning)
+        {
             Quaternion rotation = Quaternion.LookRotation(Position - transform.position, Vector3.forward);
 
             rotation.x = 0f;
             rotation.z = 0f;
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime + 10);
-        //}
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime );
+
+            Vector2 toVector = new Vector2(Position.x,Position.z) - new Vector2(transform.position.x,transform.position.z);
+            float angleToTarget = Vector2.Angle(transform.up, toVector);
+
+            if (transform.rotation.y> angleToTarget-11 && transform.rotation.y < angleToTarget + 11)
+            {
+                Turning = false;
+                Debug.Log("yo!");
+            }
+
+        }
     }
 
     private void LocatePosition()
@@ -58,6 +98,16 @@ public class BoatController : MonoBehaviour
         {
             Position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
             Debug.Log(Position);
+            target.transform.position = Position;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            Turning = false;
+        }
+    }
+
 }
