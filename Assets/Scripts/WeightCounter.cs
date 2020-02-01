b09d47d9605980f8d5c30398e236d7c2b2166fd9
Ryan Condron <rebelwebdevelopment@gmail.com>
2020-02-01 08:44:07 -0600

    added weight bar logic

---
+++ Assets/Scripts/WeightCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeightCounter : MonoBehaviour
+{
+    public double MaxWeight;
+    public double CurrentWeight;
+
+    public Slider WeightBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxWeight = 10000d;
+        CurrentWeight = 10d;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            PickUpTrash(5);
+        }
+    }
+
+    void PickUpTrash(double weight)
+    {
+        CurrentWeight += weight;
+    }
+
+
+}
