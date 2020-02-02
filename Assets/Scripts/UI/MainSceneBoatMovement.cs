9d009e27f572c7899119a0cdbe4ef118213f746e
Joey Meyer <joeymeyer1986@gmail.com>
2020-02-02 11:03:39 -0600

    updated title scene

--- Assets/Scripts/UI/MainSceneBoatMovement.cs
+++ Assets/Scripts/UI/MainSceneBoatMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainSceneBoatMovement : MonoBehaviour
 {
+    float Speed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class MainSceneBoatMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate((transform.forward * UnityEngine.Time.deltaTime) * Speed);
     }
 }
